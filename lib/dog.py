af6f1492a97af9b0d82274ec38650ea7e1a80df0
vincent44-art <vincent.murithi@student.moringaschoo.com>
2025-05-16 12:55:15 +0300

    commit

--- lib/dog.py
+++ lib/dog.py
@@ -11,29 +11,43 @@ APPROVED_BREEDS = [
 
 
 class Dog:
-    def __init__(self, name='Fido', breed='Mastiff'):
+    def __init__(self, name='Fido', breed='Mastiff', age=1):
         self.name = name
         self.breed = breed
+        self.age = age
 
-    def get_name(self):
+    @property
+    def name(self):
+        """The name property"""
         return self._name
 
-    def set_name(self, name):
+    @name.setter
+    def name(self, name):
         if isinstance(name, str) and 1 <= len(name) <= 25:
-            self._name = name.title()
+            self._name = name
         else:
-            raise ValueError(
-                "Name must be string between 1 and 25 characters.")
+            raise ValueError("Name must be string between 1 and 25 characters.")
 
-    name = property(get_name, set_name)
-
-    def get_breed(self):
+    @property
+    def breed(self):
+        """The breed property"""
         return self._breed
 
-    def set_breed(self, breed):
+    @breed.setter
+    def breed(self, breed):
         if breed in APPROVED_BREEDS:
             self._breed = breed
         else:
             raise ValueError("Breed must be in list of approved breeds.")
 
-    breed = property(get_breed, set_breed)
+    @property
+    def age(self):
+        """The age property"""
+        return self._age
+
+    @age.setter
+    def age(self, age):
+        if isinstance(age, int) and 0 <= age <= 20:
+            self._age = age
+        else:
+            raise ValueError("Age must be an integer between 0 and 20.")
--- testing/dog_property_test.py
+++ testing/dog_property_test.py
@@ -1,31 +1,16 @@
 from dog import Dog
 import pytest
 
+def test_age_is_valid_range():
+    '''validates age property is an integer between 0 and 20'''
+    dog = Dog("Buddy", "Beagle", 5)
+    assert dog.age == 5
 
-class TestDogProperties:
-    '''Class Dog in dog.py'''
+    with pytest.raises(ValueError):
+        dog.age = -1  # too low
 
-    def test_name_breed_valid(self):
-        '''validates name and breed properties are initialized with valid values'''
-        try:
-            # No exception should be thrown since name and breed are valid
-            dog = Dog("Fido", "Corgi")
-        except Exception as exc:
-            # The assertion fails if an exception is thrown
-            assert False, f'Dog("Fido", "Corgi") raised an exception {exc}'
+    with pytest.raises(ValueError):
+        dog.age = 21  # too high
 
-    def test_name_is_string_valid_length(self):
-        '''validates name property is assigned a string between 1 and 25 characters'''
-        dog = Dog("Fido", "Corgi")
-        with pytest.raises(ValueError):
-            dog.name = 7  # not a string
-        with pytest.raises(ValueError):
-            dog.name = ''  # too short
-        with pytest.raises(ValueError):
-            dog.name = 'Fido the adorable Corgi who likes to steal socks'  # too long
-
-    def test_breed_is_approved_breed(self):
-        '''validates breed property is in list of approved choices'''
-        dog = Dog("Snoopy", "Beagle")
-        with pytest.raises(ValueError):
-            dog.breed = "Poodle"  # not an approved breed
+    with pytest.raises(ValueError):
+        dog.age = "five"  # not an integer
